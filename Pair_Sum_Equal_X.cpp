3ba12694162e62d36b8bf86b39b93a4abd30cdda
Ajay Sankar <ajay0196@gmail.com>
2017-04-23 12:38:53 +0530

    Ading Triplets to Pair_Sum_Equal_X.cpp

--- Pair_Sum_Equal_X.cpp
+++ Pair_Sum_Equal_X.cpp
@@ -7,6 +7,45 @@
 #include <algorithm>
 using namespace std;
 #define MAX 10000
+void findTriplets(int arr[], int n)
+{
+    bool found = false;
+ 
+    // sort array elements
+    sort(arr, arr+n);
+ 
+    for (int i=0; i<n-2; i++)
+    {
+        // initialize left and right
+        int l = i + 1;
+        int r = n - 1;
+        int x = arr[i];
+        while (l < r)
+        {
+            if (x + arr[l] + arr[r] == 0)
+            {
+                // print elements if it's sum is zero
+                printf("%d %d %d\n", x, arr[l], arr[r]);
+                l++;
+                r--;
+                found = true;
+            }
+ 
+            // If sum of three elements is less
+            // than zero then increment in left
+            else if (x + arr[l] + arr[r] < 0)
+                l++;
+ 
+            // if sum is greater than zero than
+            // decrement in right side
+            else
+                r--;
+        }
+    }
+ 
+    if (found == false)
+        cout << " No Triplet Found" << endl;
+}
 void FindPairBySorting(int *arr,int n,int sum){
     int l=0;
     int r=n-1;
