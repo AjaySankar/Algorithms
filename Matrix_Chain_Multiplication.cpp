16d74c0a64b8c3fdff05442d4c44fd2822f31e66
Ajay Sankar <ajay0196@gmail.com>
2018-08-15 11:29:36 +0530

    Added Tabulation for matrix chain multiplication

--- Matrix_Chain_Multiplication.cpp
+++ Matrix_Chain_Multiplication.cpp
@@ -1,14 +1,17 @@
 #include<iostream>
 #include<bits/stdc++.h>
 #include<string>
+#include<chrono>
 using namespace std;
-int n = 6;
-int p[7] = {30,35,15,5,10,20,25};
-int cache[7][7];
-int split[7][7];
-int scalarCount(int i,int j);
+#define N 6
+int p[N+1] = {30,35,15,5,10,20,25};
+int cache[N+1][N+1];
+int split[N+1][N+1];
+int table[N+1][N+1];
+int scalarCountMem(int i,int j);    //By memorization technique
+int scalarCountTab();    //By Tabulation technique
 
-int scalarCount(int i,int j){
+int scalarCountMem(int i,int j){
     if(cache[i][j] != -1){
         return cache[i][j];
     }
@@ -19,7 +22,7 @@ int scalarCount(int i,int j){
     int min = INT16_MAX;
     int splitAt  = -1;
     for(int k=i;k<j;k++){
-        int temp = scalarCount(i,k)+scalarCount(k+1,j)+p[i-1]*p[k]*p[j];
+        int temp = scalarCountMem(i,k)+scalarCountMem(k+1,j)+p[i-1]*p[k]*p[j];
         if(temp < min){
             min = temp;
             splitAt = k;
@@ -28,6 +31,22 @@ int scalarCount(int i,int j){
     cache[i][j] = min;
     split[i][j] = splitAt;
     return min;
+}
+
+int scalarCountTab(){
+    for(int length=1;length<N;length++){
+        int i=1;int j=i+length;
+        for(;i<=N-length;i++,j=i+length){
+            int min = INT16_MAX;
+            for(int k=i;k<j;k++){
+                int temp = table[i][k]+table[k+1][j]+p[i-1]*p[k]*p[j];
+                if(temp < min)
+                    min = temp;
+            }
+            table[i][j] = min;
+        }
+    }
+    return table[1][N];
 }
 
 std::string printSplits(int lower,int upper){
@@ -41,13 +60,23 @@ std::string printSplits(int lower,int upper){
 }
 
 int main(){
-    for (int i = 0; i < n+1; i++){
-        for (int j = 0; j < n+1; j++){
+    for (int i = 0; i < N+1; i++){
+        for (int j = 0; j < N+1; j++){
             cache[i][j] = -1;
             split[i][j] = -1;
+            table[i][j] = 0;
         }
     }
-    cout << "NUmber of multiplications needed " << scalarCount(1,n) << endl;
-    cout << printSplits(1,n);
+    auto start = std::chrono::high_resolution_clock::now();
+    cout << "Number of multiplications needed by memorization "  << scalarCountMem(1,N) << endl;
+    auto stop = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+    std::cout << "Elapsed time: " << elapsed.count() << " s\n";
+    start = std::chrono::high_resolution_clock::now();
+    cout << "Number of multiplications needed by tabulation "  << scalarCountTab() << endl; 
+    stop = std::chrono::high_resolution_clock::now();
+    elapsed = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+    std::cout << "Elapsed time: " << elapsed.count() << " s\n";
+    cout << printSplits(1,N);
     return 0;
 }
